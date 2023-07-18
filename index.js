986bf8bfdc355e902f926a959653a753b583ab71
Twist <pranavpraveen0@gmail.com>
2023-07-18 21:21:14 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Developer", "Python developer", "Server Administrator" , "Web Developer", "Web Designer", "Freelancer", "Programmer", "Operating System Developer"];
+const textArray = ["Developer", "Python developer", "Server Administrator" , "Web Developer", "Freelancer", "Programmer", "Operating System Developer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
